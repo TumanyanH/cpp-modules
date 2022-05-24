8a0319bfeeb2df0abd660e5c82f0d8eb0bbb86fe
Hakob Tumanyan <htumanya@c2r3s5.42yerevan.am>
2022-05-24 21:46:07 +0400

    something in the way

--- module06/ex02/main.cpp
+++ module06/ex02/main.cpp
@@ -7,6 +7,7 @@
 Base * generate(void)
 {
     int a = rand() % 3;
+    std::cout << a << std::endl;
     Base *ab;
     switch (a)
     {
@@ -27,18 +28,40 @@ Base * generate(void)
 
 void identify(Base* p)
 {
-    Base *checking;
-
-    try {
-        checking = reinterpret_cast<A*>(p);
-        std::cout << "this" << std::endl;
-    } catch(...)
-    {
-        std::cout << "err" << std::endl;
-    }
+    if (dynamic_cast<A *>(p))
+        std::cout << "A" << std::endl;
+    else if (dynamic_cast<B *>(p))
+        std::cout << "B" << std::endl;
+    else if (dynamic_cast<C *>(p))
+        std::cout << "C" << std::endl;
+    else
+        std::cout << "unknown" << std::endl;
 }
 
-void identify(Base& p);
+void identify(Base& p)
+{
+    Base a;
+
+    try {
+        a = dynamic_cast<A&>(p);
+        std::cout << "A" << std::endl;
+        return ;
+    } catch(...) {}
+
+    try {
+        a = dynamic_cast<B&>(p);
+        std::cout << "B" << std::endl;
+        return ;
+    } catch(...) {}
+    
+    try {
+        a = dynamic_cast<C&>(p);
+        std::cout << "C" << std::endl;
+        return ;
+    } catch(...) {}
+    std::cout << "unkknown" << std::endl;
+    return ;
+}
 
 int main()
 {
@@ -46,8 +69,12 @@ int main()
 
     for (int a = 0; a < 10; a++)
     {
+        std::cout << "---------------" << std::endl;
         thing = generate();
+        std::cout << "Type of class is using pointer method: ";
         identify(thing);
+        std::cout << "Type of class is using reference method: ";
+        identify(*thing);
         delete thing;
     }
 }
